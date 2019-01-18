808dad0c745c80b0908dc40208a19e1ec7f736ee
Lucas777701 <45185389+Lucas777701@users.noreply.github.com>
2019-01-18 13:52:32 -0300

    Create radio.js

---
+++ radio.js
@@ -0,0 +1,23 @@
+[
+
+    {"id":11,"name":"Coca-Cola FM","url":"https://ic.imusicaradios.com.br/brasil.stream.http","pais":"Brasil"},
+    {"id":12,"name":"Mix FM","url":"http://mixmp3.crossradio.com.br:1102/live","pais":"Brasil"},
+    {"id":13,"name":"Metropolitana FM","url":"http://ice.paineldj3.com.br:8004/stream","pais":"Brasil"},
+    {"id":14,"name":"Jovem Pan FM","url":"http://servidor26.brlogic.com:8008/live","pais":"Brasil"},
+    {"id":15,"name":"Radio Hunter - The Country Channel","url":"http://live.hunterfm.com/country","pais":"Brasil"},
+    {"id":16,"name":"Radio Hunter - The Hitz Channel","url":"http://listen.shoutcast.com/RadioHunter-TheHitzChannel","pais":"Brasil"},
+    {"id":17,"name":"Radio Hunter - The Pop2K Hits Channel","url":"https://live.hunter.fm/pop2k","pais":"Brasil"},
+    {"id":19,"name":"Radio Hunter - The Fresh Channel","url":"https://live.hunter.fm/fresh","pais":"Brasil"},
+    
+    
+    {"id":1, "name":"Radio Grodno", "url":"http://178.124.203.233:8000/ogg","pais":"EUA"},
+    {"id":2, "name":"RMFMaxxx", "url":"http://217.74.72.4:8000/rmf_maxxx","pais":"EUA"},
+    {"id":3, "name":"Radio Plus (ESKA)", "url":"https://juby210.com.pl/radio.php?n=radio-plus","pais":"EUA"},
+    {"id":4, "name":"Radio ZET", "url":"http://zetpl1-01.cdn.eurozet.pl:8446/","pais":"EUA"},
+    {"id":5, "name":"ZET Party", "url":"http://zetpar-02.cdn.eurozet.pl:8444/","pais":"EUA"},
+    {"id":6, "name":"ZET Hits", "url":"http://zethit-01.cdn.eurozet.pl:8428/","pais":"EUA"},
+    {"id":7, "name":"ZET Hot", "url":"http://zet.cdn.eurozet.pl/ZETHOT.mp3","pais":"EUA"},
+    {"id":8, "name":"ZET Gold", "url":"http://zgl.cdn.eurozet.pl/ZGLHIT.mp3","pais":"EUA"},
+    {"id":10, "name":"I Love", "url":"http://stream01.iloveradio.de/iloveradio1.mp3","pais":"EUA"}
+
+]
